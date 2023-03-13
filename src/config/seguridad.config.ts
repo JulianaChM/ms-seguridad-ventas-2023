23e1790c30e039b186572cda59c37f2836a4bb5b
JulianaChM <julianchamorromarin@gmail.com>
2023-03-13 11:39:12 -0500

    Creación permisos de un rol en los menús y ajustes en la strategy y seguridad-usuario.service

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad{
   export const claveJWT = "Admin@2023*";
+  export const menuUsuarioId = "640ce9a79c5d4d339c18321d";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
